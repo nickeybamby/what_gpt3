9a6471d84d4b8d471aaa4d4f02d7fd0eb6e27a27
nicholas <nicholasobazei@gmail.com>
2024-05-01 08:14:12 +0100

    initial commit

---
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './whatGPT3.css';
+
+const WhatGPT3 = () => {
+  return (
+    <div className='gpt3__whatgpt3 section__margin' id='wgpt3' >
+       <div className='gpt3__whatgpt3-features'>
+
+       </div>
+    </div>
+  )
+}
+
+export default WhatGPT3
