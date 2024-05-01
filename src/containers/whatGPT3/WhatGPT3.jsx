10a44e094289645fdbf15415431c9da3903036d4
nicholas <nicholasobazei@gmail.com>
2024-05-01 08:55:49 +0100

    initial commit

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import './whatGPT3.css';
+import { Feature } from '../../components';
+import Features from '../features/Features';
 
 const WhatGPT3 = () => {
   return (
     <div className='gpt3__whatgpt3 section__margin' id='wgpt3' >
-       <div className='gpt3__whatgpt3-features'>
-
+       <div className='gpt3__whatgpt3-feature'>
+        <Features />
+       </div>
+       <div className='gpt3__whatgpt3-heading'>
+        <h1 className='gradient-text'>The possibilities are beyond your imagination</h1>
+        <p>Explore The Library</p>
+       </div>
+       <div className='gpt3__whatgpt3-container'>
+        <Features />
+        <Features />
+        <Features />
        </div>
     </div>
   )
